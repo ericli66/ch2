30fb95bafdec55194747a543e29efa89e5991142
ericli66 <2695625@learn.vsb.bc.ca>
2026-09-14 13:24:18 -0700

    Add files via upload

---
+++ Date.java
@@ -0,0 +1,10 @@
+public class Date{
+	public static void main(String[] args) {
+		String day = "Thursday";
+		int date = 18;
+		String month = "July";
+		int year = 2019;
+			System.out.println(day + ", " + month + " " + date + ", " + year);
+			System.out.print(day + " " + date + " " + month + " " + year);
+	}
+}
